2c7c581fb99a93128dc1b0e01b11808e3e0c100a
sankalp srivatava <Sankalp2015415@akgec.ac.in>
2022-06-04 21:08:22 +0530

    day 2 completed

---
+++ lib/Homepage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+  final int age = 5;
+  final String name = "groot";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My app"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('I am $name and my fucking age is $age'),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
